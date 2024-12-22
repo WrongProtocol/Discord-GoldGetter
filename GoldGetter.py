bb82a988e90eb02774406bfd54532b0ff77199fd
Carmine Silano <carmine@cheersounds.com>
2024-12-22 03:08:12 -0500

    enhanced the supported syntax for fractional spot prices

--- GoldGetter.py
+++ GoldGetter.py
@@ -16,7 +16,7 @@ intents.message_content = True
 
 # Set up the bot with a command prefix of '!' and specify the intents
 bot = commands.Bot(command_prefix='!', intents=intents)
-TITLE_TEXT = "*data from Summit Metals*\n"
+SUB_TEXT = "*data from Summit Metals*\n"
 
 # Function to get the gold price
 def get_gold_price():
@@ -50,6 +50,15 @@ def get_gold_price():
         # Return an error message if there is an issue with the request
         return {"error": str(e)}
 
+# Python does this crazy thing where decimals like .5 fails the .isnumeric() test. 
+# instead, we have to use this function to test it out......
+def is_float(string):
+    try:
+        float(string)  # Try converting the string to a float
+        return True
+    except ValueError:
+        return False
+    
 # Function to get the exchange rate for the specified currency
 def get_exchange_rate(to_currency):
     try:
@@ -105,19 +114,30 @@ def format_spot_price_message(price_data, currency_code="USD"):
         f"Gold Bid: {gBid} {currency_code}\n"
         f"Gold Change: {gChange} {currency_code}\n"
     #    f"Gold Change Percent (%usd): {price_data['gold_change_percent']}%\n"
-        f"{TITLE_TEXT}"
+        f"{SUB_TEXT}"
     )
 
 @bot.event
 async def on_ready():
     # Notify when the bot has connected to Discord
-    print(f'{bot.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!',  flush=True)
 
-@bot.command(name='gold', help='Get the current gold spot price. With the optional arguments, this function can be used to get spot prices in another currency, e.g. !spot CAD.  Also, as a spot calculator.  For example, !gold 1350 .5 would tell you the premium of a 1/2 oz compared to current spot price. You can also specify a currency, e.g. !gold 2000 .5 CAD')
+@bot.command(name='spot', help='use !gold at this time')
+async def spot(ctx):
+    await ctx.send("Use **!gold** to get the spot price of gold. ")
+
+@bot.command(name='gold', help='!gold gets spot price. !gold .5 would tell you the spot price for a half oz.  !gold 1350 .5 would calculate how much over/under that price is for .5oz. You can also specify a currency as the last parameter of any of these commands, e.g. !gold CAD,   !gold .5 CAD    or  !gold 2000 .5 CAD')
 async def gold(ctx, 
-               param1: str = commands.parameter(default=None, description="The price you're checking"), #param1 is listed as a str because of the use case of !gold CAD which we have to check for
-               param2: float = commands.parameter(default=None, description="The weight you're checking in decimal format. ex: 1/10th would be .1"),
-               param3: str = commands.parameter(default="USD", description="The currency code. ex: USD, CAD")):
+               param1: str = commands.parameter(default=None, description=""), #param1 is listed as a str because of the use case of !gold CAD which we have to check for
+               param2: str = commands.parameter(default=None, description=""), #param2 is listed as a STR because it could also be used as a currency in the example of !gold .1 USD
+               param3: str = commands.parameter(default="USD", description="")):
+
+# TODO THIS FUNCTION STARTED OUT SO SIMPLE, BUT FEATURE-CREEP TURNED IT MESSY. REWRITE HOW ALL THE PARAMETERS ARE HANDLED
+# OR BREAK THIS OUT INTO A FEW DIFFERENT COMMANDS. 
+
+    # print out the request for debug purposes
+    print("p1 ", param1, ", p2 ", param2, ", p3 ", param3,  flush=True)
+
     # Retrieve the current gold price data
     price_data = get_gold_price()
 
@@ -138,8 +158,34 @@ async def gold(ctx,
     if param1 is not None and param1.isalpha():
         currency_code = param1
 
-    # If both parameters (price and weight) are provided, calculate the spot difference
-    if param1 is not None and param2 is not None:
+    # there is a case where someone writes something like "!gold .25" or "!gold .25 CAD"
+    # where they want to see the current spot price for that fractional weight. 
+    if param1 is not None and is_float(param1) and (param2 is None or (isinstance(param2, str) and not is_float(param2))):
+
+        theweight = float(param1)
+        thecurrencycode = param2
+
+        #if param2 is None (no currency defined), use USD
+        if thecurrencycode is None: thecurrencycode = "USD"
+
+        # Get the conversion rate for the specified currency
+        convRate = 1 if thecurrencycode == "USD" else get_exchange_rate(thecurrencycode)
+
+        # convert to specified currency , and apply fractional weight
+        priceout = round((price_data['gold_bid'] * convRate) * theweight, 2)
+
+        await ctx.send(
+            f'**{theweight}oz** bid is at **{priceout} {thecurrencycode}**\n'
+            f"{SUB_TEXT}"
+        )
+        return
+
+    # If both parameters (price and weight) are provided and are Not None and are both numeric, then the user wants us to calculate the spot difference
+    if ((param1 is not None and 
+        (is_float(param1)))
+         and (param2 is not None and 
+         (is_float(param2)))):
+
         # Get the conversion rate for the specified currency
         rate = 1 if currency_code == "USD" else get_exchange_rate(currency_code)
         if rate is None:
@@ -147,12 +193,12 @@ async def gold(ctx,
             return
         # Convert the gold bid price to the specified currency
         gold_bid_converted = price_data['gold_bid'] * rate
-        result = calculate_spot_difference(float(param1), param2, gold_bid_converted)
+        result = calculate_spot_difference(float(param1), float(param2), gold_bid_converted)
         await ctx.send(
             f'**{param2}oz** @ **{param1} {currency_code}** '
             f'is **{result["price_diff"]} {currency_code} {result["above_or_below"]}** the spot of {result["weighted_price"]} {currency_code} for {param2}oz\n'
             f'**{result["percent_over"]}% {result["over_or_under"]}**\n'
-            f"{TITLE_TEXT}"
+            f"{SUB_TEXT}"
         )
     else:
         # If no parameters are provided, just send the current spot price
